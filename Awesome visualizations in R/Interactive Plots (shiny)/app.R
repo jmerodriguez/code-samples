89574c6b871f170538edda0c09c20a1d8a7ed3dc
jmerodriguez <jmerodriguez@users.noreply.github.com>
2021-04-05 15:09:51 -0500

    data wrangling and shape file for shiny

---
+++ Awesome visualizations in R/Interactive Plots (shiny)/app.R
@@ -0,0 +1,144 @@
+#The idea for this app was taken from https://shiny.rstudio.com/gallery/superzip-example.html
+#However, code is my own.
+#This app uses data from the United States Census Bureau to map some
+#US states' socioeconomic variables and their relation to States'
+#Medicaid affiliation rates.
+#This app is a modified version of my final project for the course Data & Programming II
+#(Prof. Jeffrey Levy) at the Harris School of Public Policy.
+
+#install.packages("leaflet")
+#install.packages("tmap")
+#install.packages("sf")
+#install.packages("shinyWidgets")
+library(shiny)
+library(leaflet)
+library(shinyWidgets)
+library(sf)
+library(tidyverse)
+library(tmap)
+
+df <- st_read("project_data.shp") %>%
+  rename("Median household size" = "hs_size",
+         "% of households with children under 18" = "under18",
+         "% of households with adults over 60" = "over60",
+         "% of households living alone" = "alone",
+         "% of residents with high school degree or higher" = "HSch",
+         "% of residents with bachelor degree or higher" = "BachDeg",
+         "Median household income" = "med_inc",
+         "Unemployment Rate" = "unempl") %>%
+  st_drop_geometry() %>%
+  select(-NAME, -state_code, -gov_party, -year, -GEOID, -medic) 
+
+
+ui <- fluidPage(
+  
+  fluidRow(h1("American Community Survey Visualizer")),
+  
+ column(width = 6.5,
+        tags$style(type = "text/css", "#USmap {height: calc(100vh - 80px) !important;}"),
+        leafletOutput(outputId = "USmap")),
+
+ absolutePanel(id = "options",
+               class = "panel panel-default",
+               
+               fluidRow(align = "center",
+                        tags$h2("Variable Explorer")),
+               
+               fluidRow(align = "center",
+                        column(width = 6,
+                               selectInput(inputId = "var",
+                                              label   = "Variable",
+                                              choices = colnames(df_mod))),
+                        column(width = 6,
+                               selectInput(inputId = "year",
+                                           label   = "Year",
+                                           choices = c(2017, 2018, 2019)))
+                        ),
+               
+               fluidRow(
+                 column(width = 6,
+                        plotOutput("histog")),
+                 column(width = 6,
+                        plotOutput("corr_med"))),
+              
+               top    = 120,
+               left   = "auto",
+               right  = 40,
+               bottom = "auto",
+               width  = 700,
+               height = "auto")
+)
+
+
+server <- function(input, output) {
+  
+   data1 <- reactive({
+
+     df %>%
+       st_drop_geometry() %>%
+       filter(year == input$year) %>%
+       select(input$var, medic, gov_party)
+  })
+  
+   data2 <- reactive({
+     
+     df %>%
+       filter(year == input$year) %>%
+       select(input$var, state_code)
+   })
+   
+
+  output$USmap <- renderLeaflet({
+
+    tmap_mode("view")
+    
+      map <- tm_shape(shp = data2()) +
+       tm_fill(col = input$var,
+               title = input$var,
+               n = 4,
+               style = "jenks",
+               alpha = 0.7,
+               popup.vars = c(input$var))  +
+       tm_borders() +
+       tm_view(view.legend.position = c("left","bottom")) +
+       tm_basemap(server = "OpenStreetMap", alpha = 0.7)
+      
+      tmap_leaflet(map) %>%
+        setView(lng = -65, lat = 37.45, zoom = 3.5)
+  })
+  
+  
+ output$histog <- renderPlot({
+   ggplot(data1()) +
+     geom_histogram(aes(x = data1()[, input$var], fill = gov_party),
+                    position = "dodge") +
+     labs(title    = "Histogram for Selected Variable",
+          subtitle = input$year,
+          x = input$var) +
+     theme_minimal() +
+     theme(plot.title    = element_text(hjust = 0.5, size = 15, face = "bold"),
+           plot.subtitle = element_text(hjust = 0.5, size = 14, face = "bold"),
+           axis.title    = element_text(size = 14),
+           legend.position = "none") +
+           scale_fill_manual(values = c("blue", "red"))
+   })
+          
+          
+output$corr_med <- renderPlot({
+  ggplot(data1()) +
+    geom_point(aes(x = data1()[, input$var], y = medic, color = gov_party)) +
+    geom_smooth(aes(x = data1()[, input$var], y = medic), method = lm, color = "black") +
+    labs(title = "Linear Fit",
+         x = input$var,
+         y = "Percentage of Residents on Medicaid",
+         color = "State\nGovernor\nParty") +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5, size = 15, face = "bold"),
+          axis.title = element_text(size = 14)) +
+          scale_color_manual(values = c("blue", "red"))
+  })
+
+}
+
+shinyApp(ui = ui, server = server)
+#https://jmerodriguez.shinyapps.io/final-project-ACS/
---
+++ Awesome visualizations in R/tidycensus data wrangling.R
@@ -0,0 +1,105 @@
+#This code downloads variables from the US Census Bureau using the tidycensus R package,
+#as well as US States geometries using the polygon data that comes in the spData R package.
+
+#install.packages("tidycensus")
+#install.packages("sf")
+library(tidycensus)
+library(tidyverse)
+library(sf)
+library(spData)
+library(glue)
+census_api_key("294056f8026e13298e326dabe95389cb23dd2fad")
+census_key <- Sys.getenv("CENSUS_API_KEY")
+
+
+###DATA WRANGLING:
+
+
+##1-year ACS
+#Table codes
+#if more tables are desired, look for codes in
+#https://data.census.gov/cedsci/all?d=ACS%201-Year%20Estimates%20Subject%20Tables
+#syntax: tableID_column
+tablesID <- c(
+  education      = "S1501_C02",
+  health_ins     = "S2704_C03",
+  unemployment   = "S2301_C04",
+  income_households = "S1901_C01",
+  household_size    = "S1101_C01" 
+)
+
+#Row numbers of the desired variables of each table
+#you search them by running the following code:
+#acs_var <- load_variables(2019, "acs1/subject", cache = TRUE)
+codes_education <- c(14:15)
+code_health_ins <- c("06")
+codes_income    <- c(12)
+codes_unemployment  <- c("01")
+codes_household_size<- c("02", 10:12)
+
+
+append_vars <- function(tableID, codes) {
+  aux2 <- c(glue('{tableID}_0{codes}'))
+  rbind(aux2)
+}
+
+
+vars <- append_vars(tablesID["education"], codes_education) %>%
+  cbind(append_vars(tablesID["health_ins"], code_health_ins),
+        append_vars(tablesID["unemployment"], codes_unemployment),
+        append_vars(tablesID["income_households"], codes_income),
+        append_vars(tablesID["household_size"], codes_household_size)
+  )
+
+var_names <- tibble(code = c("S1101_C01_002", "S1101_C01_010", "S1101_C01_011", "S1101_C01_012",
+                             "S1501_C02_014", "S1501_C02_015", "S1901_C01_012", "S2301_C04_001",
+                             "S2704_C03_006"),
+                    var_name = c("hs_size", "under18", "over60", "alone", "HSch",
+                                 "BachDeg", "med_inc", "unempl", "medic"))
+
+
+years <- c(2017, 2018, 2019)
+
+data <- list()
+i <- 1
+for (y in years) {
+  data[[i]] <- get_acs(
+    geography = "state",
+    variables = vars,
+    survey = "acs1",
+    year = y,
+    geometry = "false") %>%
+    select(-moe) %>%
+    mutate(year = y) %>%
+    pivot_wider(names_from  = "variable",
+                values_from = "estimate")
+  
+  i = i + 1
+  
+}
+
+acs_data <- rbind(data[[1]], data[[2]], data[[3]]) %>%
+  rename_at(vars(S1101_C01_002:S2704_C03_006), ~var_names$var_name) 
+  
+
+
+##US States polygons
+states_geometry <- select(us_states, GEOID, geometry)
+
+
+##Joining the two datasets
+full_df <- acs_data %>%
+  left_join(states_geometry, by = "GEOID") %>%
+  filter(NAME != "Alaska" & NAME != "Hawaii" & NAME != "Puerto Rico" & NAME != "District of Columbia") %>%
+  select(GEOID, NAME, year, geometry, everything()) %>%
+  st_as_sf(crs = 4326)
+
+
+#Run only if you want to download the data table in your computer
+#install.packages("here")
+#library(here)
+# path <- "" #directory in which you want to save the file
+# 
+# st_write(full_df,
+#          here(path, "census_data.shp"),
+#          delete_dsn = TRUE)
